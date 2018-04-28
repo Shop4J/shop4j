c9caab42b1d5562342e37832dd4e4ea620460ead
魏学东 <weixuedong@huimin100.cn>
2018-04-28 17:35:03 +0800

    完成商品搜索列表得动态局部刷新

--- src/main/java/shop4j/controllers/shop/ProductController.java
+++ src/main/java/shop4j/controllers/shop/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.expression.Strings;
 import shop4j.annotions.shop.dataload.HeadDataLoad;
 import shop4j.models.products.ProductType;
 import shop4j.models.products.SearchMoney;
@@ -17,6 +18,7 @@ import shop4j.services.products.YearOldService;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * @Author: weixuedong
@@ -37,16 +39,28 @@ public class ProductController {
 
     @Autowired
     private SearchMoneyService searchMoneyService;
+
     /**
-     * 商品搜索控制器
+     * 商品搜索首页控制器
+     * @param model thymeleaf模板
+     * @param searchText 搜索文本
+     * @param productKidType 子产品类型
+     * @return 搜索商品首页
      */
     @GetMapping("/search")
     @HeadDataLoad
-    public String searchIndex(Model model, String search){
+    public String searchIndex(Model model, String searchText,Long productKidType ){
         loadCommonData(model);//加载常量数据
-        return "shop/products/product_list";
+        model.addAttribute("searchText",searchText);
+        model.addAttribute("productKidType",productKidType);
+        model.addAttribute("count",2000);
+        return "shop/products/product_search_index";
     }
 
+    /**
+     * 加载各种常量
+     * @param model
+     */
     private void loadCommonData(Model model){
         model.addAttribute("colors",colorService.findAll());
 
@@ -63,5 +77,16 @@ public class ProductController {
         List<SearchMoney> searchMoneys = searchMoneyService.findAll();
 
         model.addAttribute("searchMoneys",searchMoneys);
+    }
+
+    /**
+     * 刷新商品
+     */
+    @GetMapping("/list")
+    public String searchProduct(String searchText,Model model){
+        int radom = new Random().nextInt(1000);
+        System.out.println(radom);
+        model.addAttribute("count",radom);
+        return "shop/products/product_list :: productList";
     }
 }
